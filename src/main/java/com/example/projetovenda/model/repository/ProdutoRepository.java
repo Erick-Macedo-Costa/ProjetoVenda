c1380411dd5d94d5f47ce849116d8bae63592ef2
Erick-Macedo-Costa <erickpart28@gmail.com>
2022-10-06 20:10:25 -0300

    pegado o repository de produtos

---
+++ src/main/java/com/example/projetovenda/model/repository/ProdutoRepository.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.projetovenda.model.repository;
+
+import com.example.projetovenda.model.entity.Produto;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author erick
+ */
+@Repository
+public class ProdutoRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Produto produto){
+        em.persist(produto);
+    }
+
+    public Produto produto(Long id){
+        return em.find(Produto.class, id);
+    }
+
+    public List<Produto> produtos(){
+        Query query = em.createQuery("from Produto");
+        return query.getResultList();
+    }
+
+    public void remove(Long id){
+        Produto p = em.find(Produto.class, id);
+        em.remove(p);
+    }
+
+    public void update(Produto produto){
+        em.merge(produto);
+    }
+}
